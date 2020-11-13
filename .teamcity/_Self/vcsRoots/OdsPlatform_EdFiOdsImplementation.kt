53f5db33207babffa92b9bdabd18ccd6a2a1d7df
Stephen A. Fuqua <stephen@safnet.com>
2020-11-13 15:22:30 -0600

    name correction

--- .teamcity/_Self/vcsRoots/OdsPlatform_EdFiOdsImplementation.kt
+++ .teamcity/_Self/vcsRoots/OdsPlatform_EdFiOdsImplementation.kt
@@ -9,7 +9,7 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 
 object OdsPlatform_EdFiOdsImplementation : GitVcsRoot({
-    name = "Ed-Fi-ODS"
+    name = "Ed-Fi-ODS Implementation"
     url = "https://github.com/%github.organization%/Ed-Fi-ODS-Implementation.git"
     branch = "%git.branch.default%"
     branchSpec = "%git.branch.specification%"
